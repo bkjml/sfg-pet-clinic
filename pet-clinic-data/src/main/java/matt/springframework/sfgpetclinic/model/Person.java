90b033a03a9b975b3606e59d4b61fcd5da4d9b79
Matt <mbirhane@kifiya.et>
2023-04-03 11:26:08 +0300

    Adding the base entity. Closes #10

--- pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package matt.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
 
--- pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/PetType.java
@@ -1,4 +1,4 @@
 package matt.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 }
