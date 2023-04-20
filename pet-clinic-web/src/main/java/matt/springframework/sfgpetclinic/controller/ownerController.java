8e310cd667f94940c51fdceada9cd21832e8d197
Matt <mbirhane@kifiya.et>
2023-04-20 15:52:26 +0300

    List all owners. Closes #16

--- pet-clinic-web/src/main/java/matt/springframework/sfgpetclinic/controller/ownerController.java
+++ pet-clinic-web/src/main/java/matt/springframework/sfgpetclinic/controller/ownerController.java
@@ -1,16 +1,22 @@
 package matt.springframework.sfgpetclinic.controller;
 
+import matt.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class ownerController {
+    private final OwnerService ownerService;
 
+    public ownerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
 
-//        model.addAttribute("owners", ownerService.findAll());
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
