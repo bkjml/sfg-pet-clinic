f5e1f6c1ee39f0585dec3d9684a151cc93f209d2
Matt <mbirhane@kifiya.et>
2023-06-10 21:59:30 +0300

    Create Visit Entity and update Owner. Closes #27

--- pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package matt.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    private Set<Pet> pets;
 }
--- pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/PetType.java
@@ -1,4 +1,14 @@
 package matt.springframework.sfgpetclinic.model;
 
 public class PetType extends BaseEntity{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
