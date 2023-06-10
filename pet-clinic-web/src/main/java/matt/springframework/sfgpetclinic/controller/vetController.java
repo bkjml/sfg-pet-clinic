970f7524c23c4fb007dd70be52991ba4932d55b6
Matt <mbirhane@kifiya.et>
2023-06-10 21:36:30 +0300

    Apply master layout to vets list page. Close #21

--- pet-clinic-web/src/main/java/matt/springframework/sfgpetclinic/controller/vetController.java
+++ pet-clinic-web/src/main/java/matt/springframework/sfgpetclinic/controller/vetController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/")
 @Controller
 public class vetController {
 
@@ -14,7 +15,7 @@ public class vetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets.html", "/index.html", "/vets/index", "/vets/index.html"})
     public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
