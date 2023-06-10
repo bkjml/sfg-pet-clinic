10a86fc46343179a7b71bfc480dc119183d6aff7
Matt <mbirhane@kifiya.et>
2023-06-10 22:52:09 +0300

    added pet type service. Closes #29

---
+++ pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package matt.springframework.sfgpetclinic.services;
+
+import matt.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
