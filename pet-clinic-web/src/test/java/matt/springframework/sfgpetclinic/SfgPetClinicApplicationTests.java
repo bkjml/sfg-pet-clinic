a40ce9f79ad2ee6d9aca3682dd1c128ea659bcfe
Matt <mbirhane@kifiya.et>
2023-06-10 19:42:20 +0300

    Bug fixes.

--- pet-clinic-web/src/test/java/matt/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/matt/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,9 +1,12 @@
 package matt.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class SfgPetClinicApplicationTests {
 
 	@Test
