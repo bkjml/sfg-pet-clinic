229ff18438afad891c80bd2bb240e74428d574ee
Matt <mbirhane@kifiya.et>
2023-06-10 22:02:42 +0300

    Create Speciality entity and associated with vet. Closes #28

--- pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/matt/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package matt.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
+
+    private Set<Specialty> specialities;
 }
